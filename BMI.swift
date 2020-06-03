3b73afb3beab8b48c6c23d0d1988622b6f2f9462
AaditT <aadit.trivedi03@gmail.com>
2020-06-02 23:51:49 -0700

    Uploading Xcode project

---
+++ BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Aadit Trivedi on 6/2/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
